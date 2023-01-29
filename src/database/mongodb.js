de54e38f31daf4965047e15bd63976c623f49e91
Junior Silva <junior_silva158@hotmail.com>
2023-01-29 16:38:05 -0300

    Database location changed

--- src/database/mongodb.js
+++ src/database/mongodb.js
@@ -3,7 +3,7 @@ mongoose.set("strictQuery", true);
 
 const connectToDatabase = async () => {
   await mongoose.connect(
-    `mongodb+srv://${process.env.ATLAS_USER}:${process.env.ATLAS_PASS}@techforum.kkebhuq.mongodb.net/?retryWrites=true&w=majority`,
+    `mongodb+srv://${process.env.ATLAS_USER}:${process.env.ATLAS_PASS}@techforum.snedsy4.mongodb.net/?retryWrites=true&w=majority`,
     (error) => {
       if (error) {
         return console.log(

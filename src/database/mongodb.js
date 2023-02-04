f3bc316d47329c79867e4a5b000c93831a0dd305
Junior Silva <junior_silva158@hotmail.com>
2023-02-04 17:53:20 -0300

    Adding local connection commit

--- src/database/mongodb.js
+++ src/database/mongodb.js
@@ -3,7 +3,12 @@ mongoose.set("strictQuery", true);
 
 const connectToDatabase = async () => {
   await mongoose.connect(
+    // Connection to MongoDB Atlas. Commit this line bellow if you want to connect locally
     `mongodb+srv://${process.env.ATLAS_USER}:${process.env.ATLAS_PASS}@techforum.snedsy4.mongodb.net/?retryWrites=true&w=majority`,
+    
+    // Connection to MongoDB Locally.
+    // `mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}`,
+    // Uncommit this line above if you want to connect locally, also change the environment variables to yours 
     (error) => {
       if (error) {
         return console.log(

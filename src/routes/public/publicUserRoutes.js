525c3c11a92d5396f4e6d01965356e5d34157a33
Junior <junior_silva158@hotmail.com>
2023-02-07 09:10:45 -0300

    Temporarily changing public routes to private

--- src/routes/public/publicUserRoutes.js
+++ src/routes/public/publicUserRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const checkToken = require("../../middleware/Authorization");
 const userController = require("../../controllers/userController");
 
-router.post("/api/register", userController.register);
-router.post("/api/login", userController.login);
+router.post("/api/register", checkToken, userController.register);
+router.post("/api/login", checkToken, userController.login);
 
 module.exports = router;
